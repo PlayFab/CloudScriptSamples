4337d1e69aeb747812988f8eb5e9183b59b1ef8a
Hamza Lazaar <brice2k5@gmail.com>
2019-07-05 11:39:18 +0100

    CHANGED: EvCode was added after initial sample

--- BasicSample/basic_sample.js
+++ BasicSample/basic_sample.js
@@ -279,7 +279,7 @@ handlers.RoomEventRaised = function (args) {
     var eventData = args.Data;
     log.debug("Event Raised - Game: " + args.GameId + " Event Type: " + eventData.eventType);
 
-    switch (eventData.eventType) {
+    switch (eventData.eventType) { // use args.EvCode instead of embedding eventType in payload
         case "playerMove":
             processPlayerMove(eventData);
             break;
